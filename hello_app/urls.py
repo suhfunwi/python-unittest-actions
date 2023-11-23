8ef40eedc6505b995e8435cb135a2f3399f0158b
clara <10088152+claraj@users.noreply.github.com>
2023-11-22 19:08:10 -0600

    update URLs for Django 4

--- hello_app/urls.py
+++ hello_app/urls.py
@@ -1,9 +1,9 @@
-from django.conf.urls import url
+from django.urls import path
 
 from . import views
 
 urlpatterns = [
-    url(r'two_plus_two', views.two_plus_two, name='two_plus_two'),
-    url(r'', views.hello, name='hello'),
+    path(r'two_plus_two', views.two_plus_two, name='two_plus_two'),
+    path(r'', views.hello, name='hello'),
     
 ]
